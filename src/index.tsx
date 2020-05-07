490becf8a0aa3d3eb710c5146e98f5bfbcb9eb3b
孙佳丽 <284159969@qq.com>
2020-05-07 20:04:44 +0800

    提交

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,17 @@ import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import Home from './page/Home/Home';
+import './global/global.scss';
+
+const sizeChange = function() {
+    let domHtml = document.getElementsByTagName("html")[0];
+    let clientWidth = document.body.clientWidth || document.documentElement.clientWidth;
+    let clientHeight = document.body.clientHeight || document.documentElement.clientHeight;
+    console.log(clientWidth);
+    domHtml.style.fontSize = Math.min(clientWidth, clientHeight)/25 + "px";
+}
+sizeChange();
+window.addEventListener("resize", sizeChange);
 
 const render = () => {
     ReactDOM.render(
